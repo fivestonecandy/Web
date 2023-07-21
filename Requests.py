a4a35c1a147e60a6ccbf8c6e60aa0c61a65faa28
Junhyeok <137851194+fivestonecandy@users.noreply.github.com>
2023-07-21 14:39:05 +0900

    웹스크래핑

--- Requests.py
+++ Requests.py
@@ -1,15 +1,19 @@
 import requests
-
 res = requests.get("http://google.com")
-res.raise_for_status() #올바른 코드, html문서를 가져온거라면 문제 없이 진행하고, 아니라면 오류 발생, 이후의 동작안하고 코드 종료시킨다.
+print("응답코드 :", res.status_code)
+#res = requests
+
+res.raise_for_status()
 print("웹 스크래핑을 진행합니다.")
 
-print(len(res.text))
-print(res.text)
 
-with open("mygoogle.html", "w", encoding="utf-8") as f:
-    f.write(res.text)
 
+with open("mygogle.html", "w", encoding="utf8") as f: #with open(파일 , 쓰기모드, 인코딩형식)
+    
+    
+    
+    
+    
 
 
 #if res.status_code == requests.codes.ok:
